06cd96bae76aa59d65b4373bfbb2aed8020acfa7
usameakdass <123920332+usameakdass@users.noreply.github.com>
2023-02-04 10:55:39 +0300

    translate app
    Translater app with different languages.

---
+++ translater/languages.js
@@ -0,0 +1,12 @@
+const languages = {
+    "ar-SA":"Arabic",
+    "de-DE":"German",
+    "en-GB":"English",
+    "es-ES":"Spanish",
+    "fr-FR":"French",
+    "ru-RU":"Russian",
+    "tr-TR":"Turkish",
+    "uk-UA":"Ukranian",
+    "fi-FI":"Finnish",
+    "it-IT":"Italian",
+}
